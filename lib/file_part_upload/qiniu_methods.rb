a9077b1f7c242ae9665d1a15bad587edd7b8ad68
fushang318 <fushang318@gmail.com>
2015-11-03 16:23:14 +0800

    release 2.0.0-beta4

--- lib/file_part_upload/qiniu_methods.rb
+++ lib/file_part_upload/qiniu_methods.rb
@@ -1,8 +1,28 @@
 module FilePartUpload
   module QiniuMethods
 
-    def url
-      File.join(FilePartUpload.get_qiniu_domain, token)
+    def url(version = nil)
+      base_url = File.join(FilePartUpload.get_qiniu_domain, token)
+
+      return base_url if version == nil
+
+      image_versions = FilePartUpload.file_part_upload_config[:qiniu_image_versions]
+      return base_url if image_versions.blank?
+
+      version_info = image_versions[version.to_s]
+      return base_url if version_info.blank?
+
+      width  = version_info[:args][0]
+      height = version_info[:args][1]
+
+      case version_info[:type].to_s
+      when "resize_to_fill"
+        return "#{base_url}?imageMogr2/thumbnail/!#{width}x#{height}r/gravity/Center/crop/#{width}x#{height}"
+      when "resize_to_fit"
+        return "#{base_url}?imageMogr2/thumbnail/#{width}x#{height}"
+      else
+        return base_url
+      end
     end
 
     def path
